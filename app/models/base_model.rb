dfdf1d8eb309bbb272a316100f97f84f84efedaa
Adam Trilling <a.trilling@celmatix.com>
2014-10-30 16:08:26 -0400

    break down very complex save method - should probably move all of the indexing to a concern

--- app/models/base_model.rb
+++ app/models/base_model.rb
@@ -59,21 +59,7 @@ class BaseModel
 
     # update indexes
     changes.each do |field, change|
-      if (has_index?(field))
-        if (has_unique_index?(field))
-          redis.hdel("#{model_key}:indexes:#{field}", change.first) if change.first.present?
-          redis.hset("#{model_key}:indexes:#{field}", change.last, @id)
-        else
-          redis.zrem("#{model_key}:indexes:#{field}:map", @id) if change.first.present?
-
-          score = redis.hget("#{model_key}:indexes:#{field}:scores", change.last)
-          unless (score)
-            score = redis.incr("#{model_key}:indexes:#{field}:score_counter")
-            redis.hset("#{model_key}:indexes:#{field}:scores", change.last, score)
-          end
-          redis.zadd("#{model_key}:indexes:#{field}:map", score, @id)
-        end
-      end
+      _update_index(field, change)
     end
 
     redis.sadd("#{model_key}:all", @id)
@@ -136,6 +122,10 @@ class BaseModel
       redis.sismember("#{model_key}:unique_indexes", key)
     end
 
+    def has_nonunique_index?(key)
+      has_index?(key) && !has_unique_index?(key)
+    end
+
     def create_unique_index(key)
       redis.sadd("#{model_key}:unique_indexes", key)
       redis.sadd("#{model_key}:indexes", key)
@@ -146,7 +136,31 @@ class BaseModel
     end
   end
 
-  delegate :redis, :model_key, :has_index?, :has_unique_index?,
+  def _update_index(field, change)
+    if (has_unique_index?(field))
+      _update_unique_index(field, change)
+    elsif (has_nonunique_index?(field))
+      _update_nonunique_index(field, change)
+    end
+  end
+
+  def _update_unique_index(field, change)
+    redis.hdel("#{model_key}:indexes:#{field}", change.first) if change.first.present?
+    redis.hset("#{model_key}:indexes:#{field}", change.last, @id)
+  end
+
+  def _update_nonunique_index(field, change)
+    redis.zrem("#{model_key}:indexes:#{field}:map", @id) if change.first.present?
+
+    score = redis.hget("#{model_key}:indexes:#{field}:scores", change.last)
+    unless (score)
+      score = redis.incr("#{model_key}:indexes:#{field}:score_counter")
+      redis.hset("#{model_key}:indexes:#{field}:scores", change.last, score)
+    end
+    redis.zadd("#{model_key}:indexes:#{field}:map", score, @id)
+  end
+
+  delegate :redis, :model_key, :has_index?, :has_unique_index?, :has_nonunique_index?,
     to: :class
 
   def object_key
