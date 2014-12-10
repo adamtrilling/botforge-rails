ad89092ce6211d769e9c92d6c73f0c1304cacba3
Adam Trilling <adamtrilling@gmail.com>
2014-12-09 22:27:23 -0500

    moar feature specs

--- spec/features/match_spec.rb
+++ spec/features/match_spec.rb
@@ -1,14 +1,33 @@
 require 'rails_helper'
 
-feature 'View a completed match' do
-  Given 'There is a completed match'
-  When 'I view it'
-  Then 'I see the final game state'
-  And 'I see the scoreboard'
-  And 'I see the full list of moves'
-end
+feature 'Matches' do
+  scenario 'View a completed match' do
+    Given 'There is a completed match'
+    When 'I view it'
+    Then 'I see the final game state'
+    And 'I see the scoreboard'
+    And 'I see the full list of moves'
+  end
 
-feature 'View an in-progress match I am not participating in' do
-  Given 'There is an in-progress match'
-  
+  scenario 'View an in-progress match I am not participating in' do
+    Given 'There is an in-progress match'
+    When 'I view it'
+    Then 'I see the current game state'
+    And 'I see the scoreboard'
+    And 'I see the list of moves so far'
+    And 'I do not see hidden information'
+  end
+
+  scenario 'View an in-progress match I am participating in' do
+    Given 'I am logged in'
+    And 'There is an in-progress match that I am playing'
+    When 'I view the match'
+    Then 'I see the current game state'
+    And 'I see the scoreboard'
+    And 'I see the list of moves so far'
+    And 'I see my hidden information'
+    And 'I do not see other players hidden information'
+  end
+
+  let(:current_user) { FactoryGirl.create(:user) }
 end
