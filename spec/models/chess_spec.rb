a0d6ddff1eb1fb2e49d2802f1577ba60bf81dbfd
Adam Trilling <adamtrilling@gmail.com>
2014-12-18 08:11:58 -0500

    fix chess initial board layout

--- spec/models/chess_spec.rb
+++ spec/models/chess_spec.rb
@@ -9,50 +9,50 @@ RSpec.describe Chess, :type => :model do
         'a' => {
           '1' => { 'piece' => 'R', 'color' => 'W' },
           '2' => { 'piece' => 'P', 'color' => 'W' },
-          '7' => { 'piece' => 'R', 'color' => 'B' },
-          '8' => { 'piece' => 'P', 'color' => 'B' }
+          '7' => { 'piece' => 'P', 'color' => 'B' },
+          '8' => { 'piece' => 'R', 'color' => 'B' }
         },
         'b' => {
           '1' => { 'piece' => 'B', 'color' => 'W' },
           '2' => { 'piece' => 'P', 'color' => 'W' },
-          '7' => { 'piece' => 'B', 'color' => 'B' },
-          '8' => { 'piece' => 'P', 'color' => 'B' }
+          '7' => { 'piece' => 'P', 'color' => 'B' },
+          '8' => { 'piece' => 'B', 'color' => 'B' }
         },
         'c' => {
           '1' => { 'piece' => 'Kn', 'color' => 'W' },
           '2' => { 'piece' => 'P', 'color' => 'W' },
-          '7' => { 'piece' => 'Kn', 'color' => 'B' },
-          '8' => { 'piece' => 'P', 'color' => 'B' }
+          '7' => { 'piece' => 'P', 'color' => 'B' },
+          '8' => { 'piece' => 'Kn', 'color' => 'B' }
         },
         'd' => {
           '1' => { 'piece' => 'Q', 'color' => 'W' },
           '2' => { 'piece' => 'P', 'color' => 'W' },
-          '7' => { 'piece' => 'Q', 'color' => 'B' },
-          '8' => { 'piece' => 'P', 'color' => 'B' }
+          '7' => { 'piece' => 'P', 'color' => 'B' },
+          '8' => { 'piece' => 'Q', 'color' => 'B' }
         },
         'e' => {
           '1' => { 'piece' => 'K', 'color' => 'W' },
-          '2' => { 'piece' => 'P', 'color' => 'W' },      
-          '7' => { 'piece' => 'K', 'color' => 'B' },
-          '8' => { 'piece' => 'P', 'color' => 'B' }
+          '2' => { 'piece' => 'P', 'color' => 'W' },
+          '7' => { 'piece' => 'P', 'color' => 'B' },
+          '8' => { 'piece' => 'K', 'color' => 'B' }
         },
         'f' => {
           '1' => { 'piece' => 'B', 'color' => 'W' },
           '2' => { 'piece' => 'P', 'color' => 'W' },
-          '7' => { 'piece' => 'B', 'color' => 'B' },
-          '8' => { 'piece' => 'P', 'color' => 'B' }
+          '7' => { 'piece' => 'P', 'color' => 'B' },
+          '8' => { 'piece' => 'B', 'color' => 'B' }
         },
         'g' => {
           '1' => { 'piece' => 'Kn', 'color' => 'W' },
           '2' => { 'piece' => 'P', 'color' => 'W' },
-          '7' => { 'piece' => 'Kn', 'color' => 'B' },
-          '8' => { 'piece' => 'P', 'color' => 'B' }
+          '7' => { 'piece' => 'P', 'color' => 'B' },
+          '8' => { 'piece' => 'Kn', 'color' => 'B' }
         },
         'h' => {
           '1' => { 'piece' => 'R', 'color' => 'W' },
           '2' => { 'piece' => 'P', 'color' => 'W' },
-          '7' => { 'piece' => 'R', 'color' => 'B' },
-          '8' => { 'piece' => 'P', 'color' => 'B' }
+          '7' => { 'piece' => 'P', 'color' => 'B' },
+          '8' => { 'piece' => 'R', 'color' => 'B' }
         }
       ]
     end
