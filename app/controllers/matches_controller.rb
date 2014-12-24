1115daff5b8ded507858ec05c5037bff6f52c285
Adam Trilling <adamtrilling@gmail.com>
2014-12-23 23:48:46 -0500

    WIP elaborate on MatchesController

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,3 +1,12 @@
 class MatchesController < ApplicationController
+  def create
+    unless Match::GAMES.has_key?(params[:game])
+      redirect_to games_path and return
+    end
 
+    @game = params[:game]
+    @match = @game.constantize.new
+
+    redirect_to match_path(@match)
+  end
 end
--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -6,17 +6,29 @@ describe MatchesController do
   describe '#create' do
     before do
       allow(controller).to receive(:current_user).and_return(current_user)
-      post :create, game: 'thermonuclear_war'
+      post :create, game: game
     end
 
-    it "assigns a new match with the current user as a participant" do
-      expect(assigns(:match)).to be_a ThermonuclearWar
-      expect(assigns(:match)).to be_persisted
-      expect(assigns(:match).participants.first.user.id).to eq current_user.id
+    context 'with a valid game' do
+      let(:game) { 'Chess' }
+
+      it 'assigns a new match with the current user as a participant' do
+        expect(assigns(:match)).to be_a Chess
+        expect(assigns(:match)).to be_persisted
+        expect(assigns(:match).participants.first.user.id).to eq current_user.id
+      end
+
+      it 'redirects to the match' do
+        expect(response).to redirect_to match_path(assigns(:match))
+      end
     end
 
-    it "redirects to the match" do
-      expect(response).to redirect_to match_path(assigns(:match))
+    context 'with an invalid game' do
+      let(:game) { 'thermonuclear_war' }
+
+      it 'redirects to the games index' do
+        expect(response).to redirect_to games_path
+      end
     end
   end
 end
